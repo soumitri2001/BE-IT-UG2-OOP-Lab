788a16de0dc5d496d148c6d25b70580e27925caf
soumitri2001 <56516406+soumitri2001@users.noreply.github.com>
2021-01-07 15:29:03 +0530

    assignment file

---
+++ 18.cpp
@@ -0,0 +1,25 @@
+#pragma GCC optimize(2)
+#pragma GCC optimize(3)
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long int ull;
+typedef long long ll;
+#define pb push_back
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //your code starts here
+
+    return 0;
+}
